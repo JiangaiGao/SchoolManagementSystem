c1cb4adae006806ada7bdb55a8664aef2ece37dd
JiangaiGao <6249771@edu.vaniercollege.qc.ca>
2023-12-11 00:06:39 -0500

    more documentation

--- src/main/java/org/ov/dto/SchoolManagementSystem.java
+++ src/main/java/org/ov/dto/SchoolManagementSystem.java
@@ -30,7 +30,13 @@ public class SchoolManagementSystem {
         this.departments = new Department[5];
     }
 
-    //Find
+    /**
+     * Search for a department with an id
+     *
+     * @param departmentId
+     * @return the department that matches the id
+     * @author jiangaiGao
+     */
     public Department findDepartment(String departmentId) {
         for (Department department : departments) {
             if (department != null && department.getDepartmentId().equals(departmentId)) {
@@ -40,6 +46,13 @@ public class SchoolManagementSystem {
         return null;
     }
 
+    /**
+     * Search for a student with an id
+     *
+     * @param studentId
+     * @return the student that matches the id
+     * @author jiangaiGao
+     */
     public Student findStudent(String studentId) {
         for (Student student : students) {
             if (student != null && student.getStudentId().equals(studentId)) {
@@ -49,6 +62,13 @@ public class SchoolManagementSystem {
         return null;
     }
 
+    /**
+     * Search for a course with an id
+     *
+     * @param courseId
+     * @return the course that matches the id
+     * @author jiangaiGao
+     */
     public Course findCourse(String courseId) {
         for (Course course : courses) {
             if (course != null && course.getCourseId().equals(courseId)) {
@@ -58,6 +78,13 @@ public class SchoolManagementSystem {
         return null;
     }
 
+    /**
+     * Search for a teacher with an id
+     *
+     * @param teacherId
+     * @return the teacher that matches the id
+     * @author jiangaiGao
+     */
     public Teacher findTeacher(String teacherId) {
         for (Teacher teacher : teachers) {
             if (teacher != null && teacher.getTeacherId().equals(teacherId)) {
@@ -67,7 +94,9 @@ public class SchoolManagementSystem {
         return null;
     }
 
-    //Print
+    /**
+     * Display all teachers that are not null.
+     */
     public void printTeachers() {
         System.out.println("Displaying all teachers:\n----------");
         for (Teacher teacher : teachers) {
@@ -77,6 +106,9 @@ public class SchoolManagementSystem {
         }
     }
 
+    /**
+     * Display all students that are not null.
+     */
     public void printStudents() {
         System.out.println("Displaying all students:\n----------");
         for (Student student : students) {
@@ -86,6 +118,9 @@ public class SchoolManagementSystem {
         }
     }
 
+    /**
+     * Display all departments that are not null.
+     */
     public void printDepartments() {
         System.out.println("Displaying all departments:\n----------");
         for (Department department : departments) {
@@ -95,6 +130,9 @@ public class SchoolManagementSystem {
         }
     }
 
+    /**
+     * Display all courses that are not null.
+     */
     public void printCourses() {
         System.out.println("Displaying all courses:\n----------");
         for (Course course : courses) {
@@ -104,7 +142,10 @@ public class SchoolManagementSystem {
         }
     }
 
-    //Add
+    /**
+     * Add a department in the school management system, max 5.
+     * @param department
+     */
     public void addDepartment(Department department) {
         if (departmentNum < 5) {
             //add the department
@@ -118,6 +159,10 @@ public class SchoolManagementSystem {
         }
     }
 
+    /**
+     * Add a course in the school management system, max 30.
+     * @param course
+     */
     public void addCourse(Course course) {
         if (courseNum < 30) {
             //add the course
@@ -131,6 +176,10 @@ public class SchoolManagementSystem {
         }
     }
 
+    /**
+     * Add a teacher in the school management system, max 20.
+     * @param teacher
+     */
     public void addTeacher(Teacher teacher) {
         if (teacherNum < 20) {
             //add the teacher
@@ -144,6 +193,10 @@ public class SchoolManagementSystem {
         }
     }
 
+    /**
+     * Add a student in the school management system, max 200.
+     * @param student
+     */
     public void addStudent(Student student) {
         if (studentNum < 200) {
             //add the student
@@ -157,7 +210,13 @@ public class SchoolManagementSystem {
         }
     }
 
-    //Modification
+    /**
+     * assign a teacher to a course,
+     * if there is already a teacher teaching, print a message.
+     * if the input ids are wrong, print a message.
+     * @param teacherId
+     * @param courseId
+     */
     public void modifyCourseTeacher(String teacherId, String courseId) {
         Teacher teacher = findTeacher(teacherId);
         Course course = findCourse(courseId);
@@ -174,13 +233,20 @@ public class SchoolManagementSystem {
         }
     }
 
+    /**
+     * sign up a student in a course,
+     * if student's already registered, print a message.
+     * if the course is full, print a message.
+     * if the input ids are wrong, print a message.
+     * @param studentId
+     * @param courseId
+     */
     public void registerCourse(String studentId, String courseId) {
         Student student = findStudent(studentId);
         Course course = findCourse(courseId);
 
         if (student != null && course != null) {
             if (course.getStudentNum() < 5) {
-                // Check if the student is not already registered for the course
                 boolean isAlreadyRegistered = false;
                 for (Student registeredStudent : course.getStudents()) {
                     if (registeredStudent != null && registeredStudent.getStudentId().equals(studentId)) {
@@ -217,10 +283,10 @@ public class SchoolManagementSystem {
 
         }
         return "SchoolManagementSystem {" +
-                "Students=" + studentsStr;// +
-                //", Courses=" + Arrays.toString(courses) +
-                //", Teachers=" + Arrays.toString(teachers) +
-                //", Departments=" + Arrays.toString(departments) +
-                //'}';
+                "Students=" + studentsStr +
+                ", Courses=" + courses +
+                ", Teachers=" + teachers +
+                ", Departments=" + departments +
+                '}';
     }
 }
